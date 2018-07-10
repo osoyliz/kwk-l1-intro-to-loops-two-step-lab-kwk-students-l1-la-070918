a23116031c3aff7733aba0ed40abf1a8f50ac0d5
osoyliz <lizosoy18@gmail.com>
2018-07-10 22:32:46 +0000

    automatically backed up by learn

---
+++ loop_example.rb
@@ -0,0 +1,28 @@
+# loop do
+#   puts "Now watch me whip"
+#   puts "Watch me nae nae"
+#   puts "Now watch me whip, whip"
+#   puts "Now watch me nae nae"
+#   puts "Ooooh watch me, watch me"
+#   puts "Ooooh watch me, watch me"
+#   puts "Ooooh watch me, watch me"
+# break
+# end 
+
+# counter= 1
+# while counter<11
+# puts counter
+# counter=counter+1
+# end
+
+def hungry_liz
+  mangos=0 
+  until mangos==10
+  puts "GIVE ME MY MANGOS!"
+  mangos+=1
+  end
+puts "Thank you, I have 10 mangos now and my sugar level is chill"
+end
+hungry_liz
+
+  
--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,14 +1,31 @@
 def first_steps
-  # Write a loop that outputs "Right foot back", "Left foot back",
-  # "Right foot back", and "Stop", with sleep after each
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Left foot back"
+  sleep(0.5)
+  puts"Right foot back"
+  sleep(0.5)
+  puts"stop"
+  sleep(1)
 end
 
+first_steps
 def a_few_more_steps
-  # Write a loop that outputs the first two sets of steps in the Two-Step
+  loop do
+  puts "Right foot steps right and back"
+  sleep(0.5)
+  puts "left foot crosses over right"
+  sleep(0.5)
+  puts "Right foot steps right"
+  sleep(0.5)
+  puts "stop"
+  sleep(1)
 end
+end
+a_few_more_steps
 
 def how_many_steps?
-  # Write a loop that fulfills all criteria from Part 3 of the README.md
+  
 end
 
 def break_dance
